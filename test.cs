2fb71ace77638418ec0c1f1b26115ae3e0587494
RODZ35 <145185063+RodzTV@users.noreply.github.com>
2025-02-03 09:40:18 +0800

    feat: ATM

---
+++ test.cs
@@ -0,0 +1,24 @@
+using System;
+
+public class test{
+    public static void Main(String []args){
+        Console.WriteLine("Welcome to Banko");
+        double Balance = 1000;
+        bool Run = true;
+
+        while(Run){
+           
+            Console.WriteLine("Menu");
+            Console.WriteLine("1 FOR Checking the Balance");
+            Console.WriteLine("2 FOR Deposite Money");
+            Console.WriteLine("3 FOR Withdrew Money");
+            Console.WriteLine("4 Exit the System");
+
+            switch(option){
+                case:1
+                
+            }
+        }
+        
+    }
+}
